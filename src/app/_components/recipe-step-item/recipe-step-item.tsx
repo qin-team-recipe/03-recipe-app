f8d227168e39dd6f969abb06af12d1f8e11ae971
Shirahamah <hiropi1224s@gmail.com>
2023-06-09 22:57:15 +0900

    💄 RecipeStepItemのスタイル修正

--- src/app/_components/recipe-step-item/recipe-step-item.tsx
+++ src/app/_components/recipe-step-item/recipe-step-item.tsx
@@ -1,25 +1,20 @@
 import { FC } from 'react';
 
 type Props = {
+  index: number;
   text: string;
   annotation: string;
   isLast?: boolean;
 };
 
-export const RecipeStepItem: FC<Props> = ({
-  text,
-  annotation,
-  isLast = false,
-}) => (
-  <li
-    className={`flex gap-2 px-2 py-4 border-gray-200 ${
-      isLast ? 'border-t border-b' : 'border-t'
-    }`}
-  >
-    <span>1</span>
+export const RecipeStepItem: FC<Props> = ({ index, text, annotation }) => (
+  <div className='flex gap-2 px-2 py-4 border-gray-200 border-b'>
+    <div className='h-5 w-5 min-w-[1.25rem] bg-red-200 rounded-full text-sm text-center'>
+      {index}
+    </div>
     <div className='flex-col'>
-      <p className='text-base font-bold'>{text}</p>
+      <p className='text-base'>{text}</p>
       <p className='text-sm text-gray-500'>{annotation}</p>
     </div>
-  </li>
+  </div>
 );
