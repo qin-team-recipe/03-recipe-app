298448f5615559d3c3be0b16b7bb5ba91abd7277
ryota misumi <3delta7@gmail.com>
2023-07-23 18:55:19 +0900

    recipeにも対応

--- src/app/shopping/page.tsx
+++ src/app/shopping/page.tsx
@@ -1,5 +1,23 @@
 import { Header } from '@/app/shopping/_components/header';
 import { MyMemo } from '@/app/shopping/_components/my-memo';
+import { RecipeMemo } from '@/app/shopping/_components/recipe-memo';
+
+const recipes = [
+  {
+    name: '豚汁',
+    ingredients: [
+      { name: 'キャベツ', checked: true },
+      { name: 'にんじん', checked: false },
+    ],
+  },
+  {
+    name: 'カレー',
+    ingredients: [
+      { name: 'じゃがいも', checked: false },
+      { name: 'たまねぎ', checked: false },
+    ],
+  },
+];
 
 export default function Shopping() {
   return (
@@ -8,6 +26,12 @@ export default function Shopping() {
       <div className='pt-5'>
         <MyMemo />
       </div>
+
+      {recipes.map((recipe) => (
+        <div className='pt-5' key={recipe.name}>
+          <RecipeMemo recipe={recipe} />
+        </div>
+      ))}
     </>
   );
 }
