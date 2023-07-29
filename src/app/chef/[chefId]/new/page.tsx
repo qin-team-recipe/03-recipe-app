9e648efaecca222c1571116185db32337d9c63fb
Shirahamah <hiropi1224s@gmail.com>
2023-07-29 23:02:42 +0900

    💄 #35 新着、人気レシピページ作成

---
+++ src/app/chef/[chefId]/new/page.tsx
@@ -0,0 +1,9 @@
+export default function New() {
+  return (
+    <main>
+      <div className='m-2 flex'>
+        <p>新着レシピ一覧</p>
+      </div>
+    </main>
+  );
+}
---
+++ src/app/chef/[chefId]/popular/page.tsx
@@ -0,0 +1,9 @@
+export default function Popular() {
+  return (
+    <main>
+      <div className='m-2 flex'>
+        <p>人気レシピ一覧</p>
+      </div>
+    </main>
+  );
+}
