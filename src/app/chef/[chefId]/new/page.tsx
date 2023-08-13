930994e905e9da1731f44d7831b296e65f2c6131
Shirahamah <hiropi1224s@gmail.com>
2023-08-13 22:40:01 +0900

    💄 #35 chefページ修正

--- src/app/chef/[chefId]/new/page.tsx
+++ src/app/chef/[chefId]/new/page.tsx
@@ -1,9 +1,58 @@
+import { tv } from 'tailwind-variants';
+
+import { RecipeItem } from '@/app/_components/recipe-item';
+import { Recipe } from '@/app/_types';
+
+const style = tv({
+  base: 'flex flex-wrap gap-x-3 gap-y-4 px-4 pt-2',
+});
+
 export default function New() {
   return (
     <main>
-      <div className='m-2 flex'>
-        <p>新着レシピ一覧</p>
+      <div className={style()}>
+        {recipeListItem.map((item) => (
+          <RecipeItem key={item.id} recipeItem={item} />
+        ))}
       </div>
     </main>
   );
 }
+
+const recipeListItem: Recipe[] = [
+  {
+    id: '1',
+    img: '',
+    title: 'トマトとルッコラのマルゲリータピザに合うホワイトソースグラタン',
+    chef: 'ウルトラハイパー超すごいしまぶーシェフ',
+    like: 1234,
+  },
+  {
+    id: '2',
+    img: '',
+    title: 'トマトとルッコラのマルゲリータピザに合うホワイトソースグラタン',
+    chef: 'ウルトラハイパー超すごいしまぶーシェフ',
+    like: 1234,
+  },
+  {
+    id: '3',
+    img: '',
+    title: 'グラタン',
+    chef: 'マイレシピ',
+    like: 0,
+  },
+  {
+    id: '4',
+    img: '',
+    title: 'トマトとルッコラのマルゲリータピザに合うホワイトソースグラタン',
+    chef: 'ウルトラハイパー超すごいしまぶーシェフ',
+    like: 1234,
+  },
+  {
+    id: '5',
+    img: '',
+    title: 'トマトとルッコラのマルゲリータピザに合うホワイトソースグラタン',
+    chef: 'ウルトラハイパー超すごいしまぶーシェフ',
+    like: 1234,
+  },
+];
