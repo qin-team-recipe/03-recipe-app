566c6d2c8ba95afa5ff369e6ef2013b6719a756c
Shirahamah <hiropi1224s@gmail.com>
2023-09-28 21:39:17 +0900

    #48 レシピ画面作成

--- src/app/(defaultLayout)/mypage/[id]/[recipeId]/ingredients/page.tsx
+++ src/app/(defaultLayout)/mypage/[id]/[recipeId]/ingredients/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { TbShoppingCartPlus } from 'react-icons/tb';
 import { tv } from 'tailwind-variants';
 
 import { RecipeHeader } from '@/app/(defaultLayout)/mypage/_components/edit-header';
@@ -9,14 +10,21 @@ import { ActionButton } from '@/app/_components/action-button';
 import { Button } from '@/app/_components/button';
 import { Tabs } from '@/app/_components/tabs';
 import { Badge } from '@/app/_components/ui/ui/badge';
-import { CopyButton } from '@/app/recipe/[recipeId]/_components/copy-button';
-import { RecipeStepItem } from '@/app/recipe/[recipeId]/_components/recipe-step-item';
+import { IngredientsItem } from '@/app/recipe/[recipeId]/ingredients/_components/ingredients-item';
 
 const layout = tv({
-  base: 'm-auto flex max-w-[400px] flex-col gap-5', // TODO: ここでmax-wを指定せずサイドバーと合わせて指定した方がよさそう
+  slots: {
+    base: 'm-auto flex max-w-[400px] flex-col gap-5', // TODO: ここでmax-wを指定せずサイドバーと合わせて指定した方がよさそう
+    ingredient: 'mb-2 mt-4 flex justify-between px-2',
+    text: 'text-base font-bold text-mauve-12',
+    icon: 'h-4 w-4 text-mauve-9',
+    button: 'flex items-center gap-1',
+  },
 });
 
-export default function Recipe({
+const { base, ingredient, text, icon, button } = layout();
+
+export default function Ingredients({
   params,
 }: {
   params: { id: string; recipeId: string };
@@ -35,7 +43,7 @@ export default function Recipe({
   ];
 
   return (
-    <main className={layout()}>
+    <main className={base()}>
       <RecipeHeader />
       <div className='flex items-start justify-between gap-1 self-stretch'>
         <p className='text-2xl font-bold text-mauve-12'>
@@ -56,28 +64,30 @@ export default function Recipe({
       </div>
       <div>
         <Tabs tabs={tabs} />
-        {recipeItems.map((item, i) => (
-          <RecipeStepItem key={i} index={i + 1} text={item.text} />
-        ))}
-        <div className='m-2 flex justify-end'>
-          <CopyButton recipeItems={recipeItems} />
+        <div className={ingredient()}>
+          <p className={text()}>2人前</p>
+          {/* TODO: お買い物リストに追加処理 */}
+          <button className={button()}>
+            <TbShoppingCartPlus className={icon()} />
+            <p className={text({ class: 'text-mauve-9' })}>
+              まとめてお買い物に追加
+            </p>
+          </button>
         </div>
+        {ingredients.map((ingredient, i) => (
+          <IngredientsItem
+            key={i}
+            text={ingredient.text}
+            isLast={ingredients.length - 1 === i}
+          />
+        ))}
       </div>
     </main>
   );
 }
-
-const recipeItems = [
-  {
-    text: '用意するメインの材料は、マカロニ、牛乳、鶏もも肉、玉ねぎ、椎茸で、バター、小麦粉、塩、こしょうも使用します。',
-  },
-  {
-    text: '用意するメインの材料は、マカロニ、牛乳、鶏もも肉、玉ねぎ、椎茸で、バター、小麦粉、塩、こしょうも使用します。',
-  },
-  {
-    text: '用意するメインの材料は、マカロニ、牛乳、鶏もも肉、玉ねぎ、椎茸で、バター、小麦粉、塩、こしょうも使用します。',
-  },
-  {
-    text: '用意するメインの材料は、マカロニ、牛乳、鶏もも肉、玉ねぎ、椎茸で、バター、小麦粉、塩、こしょうも使用します。',
-  },
+const ingredients = [
+  { text: 'キャベツ' },
+  { text: 'キャベツ' },
+  { text: 'キャベツ' },
+  { text: 'キャベツ' },
 ];
